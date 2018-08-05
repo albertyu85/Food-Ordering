e8c55f7e25f05621ba36b14df4f32f9d143c2ec4
Albert Yu <albertyu_85@yahoo.com>
2018-08-05 18:48:03 -0400

    refactored details page

--- app/src/main/java/com/example/albertyu/foodordering/DetailActivity.java
+++ app/src/main/java/com/example/albertyu/foodordering/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.albertyu.foodordering;
 
+import android.app.ActionBar;
 import android.content.res.Configuration;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -16,10 +17,8 @@ import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private DrawerLayout drawerLayout;
+
     private Toolbar toolbar;
-    private NavigationView navigationView;
-    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,100 +28,21 @@ public class DetailActivity extends AppCompatActivity {
 
         // since, NoActionBar was defined in theme, we set toolbar as our action bar.
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //this basically defines on click on each menu item.
-        //setUpNavigationView(navigationView);
-
-        //This is for the Hamburger icon.
-        drawerToggle = setupDrawerToggle();
-        drawerLayout.addDrawerListener(drawerToggle);
-
-        //Inflate the first fragment,this is like home fragment before user selects anything.
+        Bundle extras = getIntent().getExtras();
+        DetailFragment n = new DetailFragment();
+        n.setArguments(extras);
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.frameContent,new DetailFragment()).commit();
+        fragmentManager.beginTransaction().replace(R.id.frameContent, n).commit();
 
     }
 
     void initializeStuff(){
-        drawerLayout =(DrawerLayout) findViewById(R.id.drawerLayout);
+        //drawerLayout =(DrawerLayout) findViewById(R.id.drawerLayout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        navigationView = (NavigationView) findViewById(R.id.navigationDrawer);
+        //navigationView = (NavigationView) findViewById(R.id.navigationDrawer);
     }
 
-    /**
-     * Inflate the fragment according to item clicked in navigation drawer.
-     */
-    private void setUpNavigationView(final NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        //replace the current fragment with the new fragment.
-                        Fragment selectedFragment = selectDrawerItem(menuItem);
-                        FragmentManager fragmentManager = getSupportFragmentManager();
-                        fragmentManager.beginTransaction().replace(R.id.frameContent, selectedFragment).commit();
-                        // the current menu item is highlighted in navigation tray.
-                        navigationView.setCheckedItem(menuItem.getItemId());
-                        setTitle(menuItem.getTitle());
-                        //close the drawer when user selects a nav item.
-                        drawerLayout.closeDrawers();
-                        return true;
-                    }
-                });
-    }
 
-    /**
-     * This method returns the fragment according to navigation item selected.
-     */
-    public Fragment selectDrawerItem(MenuItem menuItem){
-        Fragment fragment = null;
-        switch(menuItem.getItemId()) {
-            case R.id.nav_item_my_account:
-                break;
-            case R.id.nav_item_my_cart:
-                break;
-            case R.id.nav_item_sign_out:
-                break;
-        }
-        return fragment;
-    }
-
-    /**
-     * This is to setup our Toggle icon. The strings R.string.drawer_open and R.string.drawer close, are for accessibility (generally audio for visually impaired)
-     * use only. It is now showed on the screen. While the remaining parameters are required initialize the toggle.
-     */
-    private ActionBarDrawerToggle setupDrawerToggle() {
-        return new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open,R.string.close);
-    }
-
-    /**
-     * This makes sure that the action bar home button that is the toggle button, opens or closes the drawer when tapped.
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    /**
-     * This synchronizes the drawer icon that rotates when the drawer is swiped left or right.
-     * Called inside onPostCreate so that it can synchronize the animation again when the Activity is restored.
-     */
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-    /**
-     * This is to handle generally orientation changes of your device. It is mandatory to include
-     * android:configChanges="keyboardHidden|orientation|screenSize" in your activity tag of the manifest for this to work.
-     */
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
 }
--- app/src/main/java/com/example/albertyu/foodordering/DetailFragment.java
+++ app/src/main/java/com/example/albertyu/foodordering/DetailFragment.java
@@ -5,9 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailFragment extends Fragment{
 
+    ImageView mImageView;
+    TextView mName;
+    TextView mDescription;
+    int i;
+    String name;
+    String description;
     public DetailFragment() {
         // Required empty public constructor
     }
@@ -20,8 +28,22 @@ public class DetailFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_detail, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        mImageView = (ImageView) view.findViewById(R.id.imageViewItem);
+        mName = (TextView) view.findViewById(R.id.textViewName);
+        mDescription = (TextView) view.findViewById(R.id.textViewDetailDescription);
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            i = bundle.getInt("Image", 0);
+            name = bundle.getString("Name");
+            description = bundle.getString("Description");
+        }
+        mImageView.setImageResource(i);
+        mDescription.setText(description);
+        mName.setText(name);
+
+        return view;
     }
 
 }
