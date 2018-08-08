f66a15985a1c2b7ab8311c68c5145833a94b844a
Albert Yu <albertyu@usolmac-415480.use.ucdp.net>
2018-08-08 10:44:29 -0400

    Revert "changed listview to gridview"
    This reverts commit 2808a235a44033006bcd6249a738d751f61ea4db.

--- app/src/main/java/com/example/albertyu/foodordering/view/IndividualFragment.java
+++ app/src/main/java/com/example/albertyu/foodordering/view/IndividualFragment.java
@@ -7,10 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.GridView;
 import android.widget.ListView;
 
-import com.example.albertyu.foodordering.adapter.ItemAdapter;
 import com.example.albertyu.foodordering.adapter.MyListAdapter;
 import com.example.albertyu.foodordering.controller.Controller;
 import com.example.albertyu.foodordering.model.ShoppingItem;
@@ -40,48 +38,15 @@ public class IndividualFragment extends Fragment{
     final Controller controller = (Controller) getActivity().getApplicationContext();
 
     shoppingItemList = new ArrayList<>();
-    //listView = (ListView) view.findViewById(R.id.listview_food);
-    GridView gridView = (GridView) view.findViewById(R.id.gridview);
+    listView = (ListView) view.findViewById(R.id.listview_food);
 
     shoppingItemList.add(new ShoppingItem(R.drawable.mackeral, "Mackerel In Tomato Sauce", "Brand: Excelsior \n Net Weight: 5.5 Oz (155g)"));
     shoppingItemList.add(new ShoppingItem(R.drawable.sardinesinoil, "Sardines In Oil", "Brand: Excelsior \n Net Weight: 3.75 Oz (106g)"));
     shoppingItemList.add(new ShoppingItem(R.drawable.vienna, "Vienna Sausages", "Made with Chicken, Beef, And Pork. Added in Chicken Broth. Hot &amp Spicy \n Brand: Excelsior \n Net Weight: 4.8 Oz"));
     shoppingItemList.add(new ShoppingItem(R.drawable.watercrackers, "Water Crackers", "Cinnamon and Fat Free \n Brand: Excelsior"));
     shoppingItemList.add(new ShoppingItem(R.drawable.porkszn, "Jamaican Pork Seasoning", "Brand: EASISPICE \n Net Weight: 3.6 Oz (130g)"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.ackees, "Ackees", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.bakedbeans, "Baked Beans", "Test Description"));
-   /* shoppingItemList.add(new ShoppingItem(R.drawable.chickenseasoning, "Chicken Seasoning", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.coconutmilkcan, "Canned Coconut Milk", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.cocunutwater, "Coconut Water", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.creamedcocunut, "Creamed Coconut", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.crushedpeppersauce, "Crushed Pepper Sauce", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.currypowder, "Curry Powder", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.fishandmeatsauce, "Fish and Meat Sauce", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.fruitpunch, "Fruit Punch", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.irishmoss, "Irish Moss", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.jerksauce, "Jerk Sauce", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.jerkseasoning, "Jerk Seasoning", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.meatseasoning, "Meat Seasoning", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.mildjerk, "Mild Jerk", "Test Description"));
-    shoppingItemList.add(new ShoppingItem(R.drawable.oxtailseasoning, "Oxtail Seasoning", "Test Description"));*/
 
-    ItemAdapter itemAdapter = new ItemAdapter(getActivity(), R.layout.grid_layout, shoppingItemList);
-
-    gridView.setAdapter(itemAdapter);
-
-    gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-      @Override
-      public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        Intent intent = new Intent(getActivity(), DetailActivity.class);
-        ShoppingItem item = (ShoppingItem) adapterView.getItemAtPosition(i);
-        intent.putExtra("Image", item.getImage());
-        intent.putExtra("Name", item.getName());
-        intent.putExtra("Description", item.getDescription());
-
-        startActivity(intent);
-      }
-    });
-    /*MyListAdapter adapter = new MyListAdapter(getActivity(), R.layout.list_layout, shoppingItemList);
+    MyListAdapter adapter = new MyListAdapter(getActivity(), R.layout.list_layout, shoppingItemList);
 
     listView.setAdapter(adapter);
 
@@ -96,7 +61,7 @@ public class IndividualFragment extends Fragment{
 
         startActivity(intent);
       }
-    });*/
+    });
 
     return view;
   }
