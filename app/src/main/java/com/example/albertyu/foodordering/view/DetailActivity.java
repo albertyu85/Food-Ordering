2a4c1fac16cb861374b7c0a47553d76e68f5adad
Albert Yu <albertyu_85@yahoo.com>
2018-08-22 11:40:14 -0700

    added FAB functionality to detail activity

--- app/src/main/java/com/example/albertyu/foodordering/view/DetailActivity.java
+++ app/src/main/java/com/example/albertyu/foodordering/view/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.albertyu.foodordering.view;
 
 import android.content.Intent;
 import android.media.Image;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -35,6 +37,15 @@ public class DetailActivity extends AppCompatActivity {
         toolbar.setTitle("Mo Jamaican LLC");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         if (getIntent() != null) {
             categoryId = getIntent().getStringExtra("CategoryId");
